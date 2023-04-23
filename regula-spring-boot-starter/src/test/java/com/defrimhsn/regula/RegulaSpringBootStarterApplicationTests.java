8bef15f4a3f6b7d60d6617c7d168a8b8c7fe71f0
defrimhasani <defrimhsn@gmail.com>
2023-04-23 13:26:27 +0200

    feat(1): Initialize Regula Repository
    Initialize repository

--- regula-spring-boot-starter/src/test/java/com/defrimhsn/regula/RegulaSpringBootStarterApplicationTests.java
+++ regula-spring-boot-starter/src/test/java/com/defrimhsn/regula/RegulaSpringBootStarterApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes ={
-        DefaultRuleEngine.class, NameStartsWithA.class
+@SpringBootTest(classes = {
+        DefaultRuleEngine.class
 })
 class RegulaSpringBootStarterApplicationTests {
 
